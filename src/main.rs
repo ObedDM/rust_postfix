ba915ac7e26fdbd53c306997e9cf6ba8f4a64094
ObedDM <pacocobot2@gmail.com>
2025-03-17 10:49:19 -0600

    Created is_right and added closing delimeter confirmation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::stdin;
+use std::{collections::HashMap, io::stdin};
 
 use regex::Regex;
 
@@ -18,12 +18,51 @@ fn main() {
 
     // Filtered by lexeme
     let re = Regex::new(r"\d+|/|\+|-|\*|\^|√|\(|\)|\[|\]|\{|\}").unwrap();
-    let matches: Vec<_> = re.find_iter(&infix).map(|m| m.as_str()).collect();
+    let matches: Vec<&str> = re.find_iter(&infix).map(|m| m.as_str()).collect();
 
     println!("{:?}", matches);
 
 }
+struct PermittedDelimiters {
+    start_del: &'static str,
+    start_counter: u8,
+    
+    end_del: &'static str,
+    end_counter: u8,
 
+    is_match: bool
+}
+
+fn is_right(expression: Vec<&str>) -> bool {
+
+    let mut parenthesis = PermittedDelimiters{ start_del: "(", end_del: ")", start_counter: 0, end_counter: 0, is_match: false };
+    let mut brackets = PermittedDelimiters{ start_del: "[", end_del: "]", start_counter: 0, end_counter: 0, is_match: false };
+    let mut braces = PermittedDelimiters{ start_del: "{", end_del: "}", start_counter: 0, end_counter: 0, is_match: false };
+
+    let mut delimiters: [&mut PermittedDelimiters;3] = [&mut parenthesis, &mut brackets, &mut braces];
+
+    for lexeme in expression {
+        for del in delimiters.iter_mut() {
+            if lexeme == del.start_del {
+                del.start_counter += 1;
+            }
+
+            if lexeme == del.end_del {
+                del.end_counter += 1;
+            }
+
+            if del.start_counter == del.end_counter {
+                del.is_match = true;
+            }
+        }
+    }
+
+    if parenthesis.is_match && brackets.is_match && braces.is_match {
+        return true;
+    }
+
+    return false   
+}
 
 /*
 entregar programa en eq:
