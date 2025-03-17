2fd3808ea499e2b064b7f94407c86f6c374c00fd
ObedDM <pacocobot2@gmail.com>
2025-03-16 22:40:06 -0600

    Added instructions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
     println!("Hello, world!");
 }
+
+
+/*
+entregar programa en eq:
+cuando el usuario le ponga una expresion infija
+1. evaluar si la expresion esta correctamente escrita
+2. si esta correctamente escrita, debe de mostrarme su notacion (dar en prefija postfija infija)
+3.- evaluar la expresion (en postfija obvio)
+4.- debe permitir agrupadores (parentesis, corchete, llave)
+5.- permitir suma resta division multiplicacion potencia raiz
+6.- permitir numeros de N digitos
+7.- debe dar igual si tienen o no espacios
+
+usar re.findall
+*/
