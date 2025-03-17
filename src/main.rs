6daa3378430846386365b408c6fb90943042e755
ObedDM <pacocobot2@gmail.com>
2025-03-17 08:57:58 -0600

    Added  brackets and braces as possible lexemes

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@ fn main() {
     // Extract lexemes from data:
 
     // Removed whitespace from infix
-    let mut infix: String = "(3+ 4545 / 3) * 64 / 1 + 2 (33 33 - 2)".to_string();    
+    let mut infix: String = "(3+ 4545 / 3) * {64} / 1 + [2] (33 33 - 2)".to_string();    
     infix = infix.split(' ').collect();
 
     // Filtered by lexeme
-    let re = Regex::new(r"\d+|/|\+|-|\*|\^|√|\(|\)").unwrap();
+    let re = Regex::new(r"\d+|/|\+|-|\*|\^|√|\(|\)|\[|\]|\{|\}").unwrap();
     let matches: Vec<_> = re.find_iter(&infix).map(|m| m.as_str()).collect();
 
     println!("{:?}", matches);
@@ -22,7 +22,7 @@ cuando el usuario le ponga una expresion infija
 1. evaluar si la expresion esta correctamente escrita
 2. si esta correctamente escrita, debe de mostrarme su notacion (dar en prefija postfija infija)
 3.- evaluar la expresion (en postfija obvio)
-4.- debe permitir agrupadores (parentesis, corchete, llave)
+4.- debe permitir agrupadores (parentesis, corchete, llave) [DONE]
 5.- permitir suma resta division multiplicacion potencia raiz
 6.- permitir numeros de N digitos
 7.- debe dar igual si tienen o no espacios [DONE]
